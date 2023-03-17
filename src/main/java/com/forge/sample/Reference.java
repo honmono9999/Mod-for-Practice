d55006bb99348ccde7569bc73c0dc81d13314305
honmono9999 <b213213@naver.com>
2023-03-17 15:08:27 +0900

    Changes ***

---
+++ src/main/java/com/forge/sample/Reference.java
@@ -0,0 +1,7 @@
+package com.forge.sample;
+
+public class Reference {
+    public static final String MODID = "forgesample";
+    public static final String NAME = "Forge_Sample";
+    public static final String VERSION = "1.0";
+}
